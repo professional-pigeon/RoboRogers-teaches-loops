cd088299f6a2998b6159badbd4e29dfb4061fae0
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-11 11:26:00 -0700

    replace any string containing a 1 with beeps.

--- js/scripts.js
+++ js/scripts.js
@@ -20,38 +20,40 @@ test: "will remove 3 from array"
 code: roboResponse(5)
 expected output: [0, 1, 2, 4, 5]
 
-test: "will remove any number with a 3 from an array"
+test: "will convert an array into a string"
+
+test: "will convert a string into a string array"
+
+test: "will remove any string with a '3' from a string array"
 code: roboResponse(14)
-expected output: [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14]
+expected output: ["0", "1", "2", "4", "5", "6", "7", "8", "9", "10", "11", "12", "14"]
+
+test: "will replace any strings containing a 3 with the phrase 'Won't you be my neighbor'"
 
 test
 
 function roboResponse(element) {
-  console.log(typeof(element)) 
 
   let numberArray = []
 
   for (i = 0; i <= element; i ++) {
-    if (element > i) {
+    if (element >= i) {
       numberArray.push(i)
     }
   }
-  console.log(numberArray.includes(3))
-  numberArray.splice(numberArray.indexOf(3), 1)
+
   stringArray = numberArray.toString()
   stringArray = stringArray.split(",")
 
   stringArray.forEach(function(string) {
-    if (string.includes(3)) {
-      stringArray.splice(stringArray.indexOf(string), 1);
+    if (string.includes("3")) {
+      stringArray.splice(stringArray.indexOf(string), 1, "Won't you be my Neighbor?");
+    } else if (string.includes("2")) {
+      stringArray.splice(stringArray.indexOf(string), 1, "Boop!");
+    } else if (string.includes("1")) {
+      stringArray.splice(stringArray.indexOf(string), 1, "Beep!");
+    } else {
     }
   });
-  return stringArray
+  console.log(stringArray)
 }
-
-numberArray.forEach(function(number) {
-  if (numberArray.includes(3, numberArray.indexOf(number))) {
-    numberArray.splice(numberArray.indexOf(number), 1)
-  }
-  return numberArray
-})
