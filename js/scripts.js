30e9859501a02c5e8ce6e989359af4f9f9a5cf53
professional-pigeon <kyle.kayperez@gmail.com>
2021-06-11 08:40:55 -0700

    pass test if function has 3 in any position.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,24 @@ test: "should tell you a number is a number"
 code: roboResponse(2)
 expected output: (number)
 
-test: "Should tell you a number includes 3"
-code: roboResponse(53)
+test: "should recognize a number is 3"
+code: roboResponse(3)
 expected output: (true)
+
+test: "should recognize a number with a 3 in any position"
+code: roboResponse(13)
+expected output: (true)
+
+test
 
 function roboResponse(element) {
   console.log(typeof(element)) 
-  if (element.includes(3)) {
+  numberString = toString(element)
+  console.log(element)
+  console.log(numberString.includes("3"))
+
+  if (true === numberString.includes("3")) {
     return true
   }
 }
+
